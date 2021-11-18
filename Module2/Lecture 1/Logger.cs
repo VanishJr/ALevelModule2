287dc1e2267be45e66a7ee8acd6e4066264fa0a4
Ivan Logutov <foreverivan2004@gmail.com>
2021-11-18 17:25:19 +0200

    Logger.Remake

---
+++ Module2/Lecture 1/Logger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Logger
+{
+    List<string> listLog = new List<string>();
+
+    private static Logger? instance;
+
+    private Logger()
+    { }
+
+    public enum TypeLog
+    {
+        Error,
+        Info,
+        Warning
+    }
+
+    public static Logger getIns()
+    {
+        if (instance == null)
+            instance = new Logger();
+        return instance;
+    }
+
+    internal void Add(TypeLog typeLog, string message)
+    {
+        var logText = $"{DateTime.Now} : {typeLog} : {message}";
+        Console.WriteLine(logText);
+        listLog.Add(logText);
+
+    }
+
+    internal string GetPath()
+    {
+        string path = @"C:\Users\forev\source\repos\ALevelModule2\Module2\Lecture 1\LogFolder\log.txt";
+        return path;
+    }
+
+    internal void WriteToFile()
+    {
+        File.WriteAllText(GetPath(), string.Join(Environment.NewLine, listLog));
+    }
+
+    internal List<string> GetLog()
+    {
+        return listLog;
+    }
+}
---
+++ Module2/Lecture 1/Starter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Starter
+{
+	internal void Run()
+	{
+		int ranLog;
+		Logger logger = Logger.getIns();
+		Result result = new Result();
+		Random random = new Random();
+		Actions actions = new Actions();
+		for (int i = 0; i < 100; i++)
+		{
+			ranLog = random.Next(1, 3);
+			switch (ranLog)
+			{
+				case 1:
+					result = actions.InputInfo();
+					break;
+				case 2:
+					result = actions.InputWarning();
+					break;
+				case 3:
+					result = actions.InputError();
+					break;
+			}
+		}
+
+		logger.WriteToFile();
+
+
+	}
+}
+
+
